62119e4669a196f7c37dcb7059802adc64de2a65
chtk3131 <takuya-ochiai@aeroasahi.co.jp>
2021-02-09 16:05:25 +0900

    状態管理用クラスを一通り作成

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -8,7 +8,6 @@
 // import 'package:build_runner/build_runner.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:architecture_bluepoints/main.dart' as app;
 
 // void main() => run(_testMain);
